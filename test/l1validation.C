a2c3491578be8f55520e91b4e509af89b877dfa3
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-06-27 14:58:18 +0000

    *** empty log message ***

--- test/l1validation.C
+++ test/l1validation.C
@@ -17,88 +17,127 @@ void l1validation()
 
   // Postscript file
   TPostScript* ps =new TPostScript("l1validation.ps", 112);
-
+  
   // Canvas
   TCanvas *c1 = new TCanvas("c1","c1",800,800);
-
-
-  // single muon plots (pt resolution, eff vs eta/phi)
-  TFile *f = new TFile("single_mu.root")
-  TString dir = 
-
-  Plot(f,dir+"/L1Candidates/Et","E_{T} (GeV)","No. of entries"); c1->Update();
-  Plot(f,dir+"/L1Candidates/Eta","#eta","No. of entries"); c1->Update();
-  Plot(f,dir+"/L1Candidates/Phi","#phi (rad)","No. of entries"); c1->Update();
-
-  f->close();
-
-  // single electron plots (et resolution, eff vs eta/phi)
-  TFile *f = new TFile("single_e.root")
-
-  Plot(f,dir+"/L1Candidates/Et","E_{T} (GeV)","No. of entries"); c1->Update();
-  Plot(f,dir+"/L1Candidates/Eta","#eta","No. of entries"); c1->Update();
-  Plot(f,dir+"/L1Candidates/Phi","#phi (rad)","No. of entries"); c1->Update();
-
-  f->close();
-
-  // single pion plots (et resolution, eff vs eta/phi)
-  TFile *f = new TFile("single_pi.root")
-
-  f->close();
-
-  // H -> 4 mu plots (turn on)
-  TFile *f = new TFile("H_4mu.root")
-
-  f->close();
-
-  // H -> 2 gamma plots (turn on)
-  TFile *f = new TFile("H_2gamma.root")
-
-  f->close();
-
-  // H -> 2 tau plots (turn on)
-  TFile *f = new TFile("H_2tau.root")
-
-  f->close();
-
-  // Z' -> dijets plots (turn on)
-  TFile *f = new TFile("ZprimeDijets.root")
-
-  f->close();
-
-}
-
-void validation(TString dir="", TString fileName="")
-{
-  // Open the file
-  TFile *f = new TFile(fileName);
-
-  // First the simple histograms
   
-  Plot(f,dir+"/RefCandidates/Et","E_{T} (GeV)","No. of entries"); c1->Update();
-  Plot(f,dir+"/RefCandidates/Eta","#eta","No. of entries"); c1->Update();
-  Plot(f,dir+"/RefCandidates/Phi","#phi (rad)","No. of entries"); c1->Update();
- 
-  Plot(f,dir+"/Resolutions/DeltaR","#Delta R (L1,Ref)","No. of entries"); c1->Update();
+  
+  //   // single muon plots (pt resolution, eff vs eta/phi)
+  //   TFile *f = new TFile("single_mu.root");
+  //   TString dir = "";
+  
+  //   // resolutions
+  //   Plot(f,dir+"/Resolutions/EtRes","(E_{T,L1}-E_{T,Ref})/E_{T,Ref}","No. of entries"); c1->Update();
+  //   Plot(f,dir+"/Resolutions/EtCor","E_{T, Ref} (GeV)","E_{T, L1} (GeV)"); c1->Update();
+  //   Plot(f,dir+"/Resolutions/EtaRes","(#eta_{L1}-#eta_{Ref})/#eta_{Ref}","No. of entries"); c1->Update();
+  //   Plot(f,dir+"/Resolutions/EtaCor","#eta_{Ref}","#eta_{L1}"); c1->Update();
+  //   Plot(f,dir+"/Resolutions/PhiRes","(#phi_{L1}-#phi_{Ref})/#phi_{Ref}","No. of entries"); c1->Update();
+  //   Plot(f,dir+"/Resolutions/PhiCor","#phi_{Ref} (rad)","#phi_{L1} (rad)"); c1->Update();
+  
+  //   // efficiencies
+  //   Plot(f,dir+"/Efficiencies/EtEff","E_{T} (GeV)","Efficiency","e"); c1->Update();
+  //   Plot(f,dir+"/Efficiencies/EtaEff","#eta","Efficiency","e"); c1->Update();
+  //   Plot(f,dir+"/Efficiencies/PhiEff","#phi (rad)","Efficiency","e"); c1->Update();
+  
+  //   f->close();
+  
+  // single electron plots (et resolution, eff vs eta/phi)
+  TFile *f = new TFile("single_e.root");
+  
+  // iso EM resolutions & efficiencies
+  TString dir = "L1AnalyzerIsoEmMC";
+  
   Plot(f,dir+"/Resolutions/EtRes","(E_{T,L1}-E_{T,Ref})/E_{T,Ref}","No. of entries"); c1->Update();
   Plot(f,dir+"/Resolutions/EtCor","E_{T, Ref} (GeV)","E_{T, L1} (GeV)"); c1->Update();
-  Plot(f,dir+"/Resolutions/EtProf","E_{T, Ref} (GeV)","(E_{T,L1}-E_{T,Ref})/E_{T,Ref}"); c1->Update();
-
   Plot(f,dir+"/Resolutions/EtaRes","(#eta_{L1}-#eta_{Ref})/#eta_{Ref}","No. of entries"); c1->Update();
   Plot(f,dir+"/Resolutions/EtaCor","#eta_{Ref}","#eta_{L1}"); c1->Update();
-  Plot(f,dir+"/Resolutions/EtaProf","#eta_{Ref}","(#eta_{L1}-#eta_{Ref})/#eta_{Ref}"); c1->Update();
-
   Plot(f,dir+"/Resolutions/PhiRes","(#phi_{L1}-#phi_{Ref})/#phi_{Ref}","No. of entries"); c1->Update();
   Plot(f,dir+"/Resolutions/PhiCor","#phi_{Ref} (rad)","#phi_{L1} (rad)"); c1->Update();
-  Plot(f,dir+"/Resolutions/PhiProf","#phi_{Ref} (rad)","(#phi_{L1}-#phi_{Ref})/#phi_{Ref}"); c1->Update();
+  Plot(f,dir+"/Efficiencies/EtEff","E_{T} (GeV)","Efficiency","e"); c1->Update();
+  Plot(f,dir+"/Efficiencies/EtaEff","#eta","Efficiency","e"); c1->Update();
+  Plot(f,dir+"/Efficiencies/PhiEff","#phi (rad)","Efficiency","e"); c1->Update();
 
+  // non-iso EM resolutions & efficiencies
+  TString dir = "L1AnalyzerNonIsoEmMC";
+  
+  Plot(f,dir+"/Resolutions/EtRes","(E_{T,L1}-E_{T,Ref})/E_{T,Ref}","No. of entries"); c1->Update();
+  Plot(f,dir+"/Resolutions/EtCor","E_{T, Ref} (GeV)","E_{T, L1} (GeV)"); c1->Update();
+  Plot(f,dir+"/Resolutions/EtaRes","(#eta_{L1}-#eta_{Ref})/#eta_{Ref}","No. of entries"); c1->Update();
+  Plot(f,dir+"/Resolutions/EtaCor","#eta_{Ref}","#eta_{L1}"); c1->Update();
+  Plot(f,dir+"/Resolutions/PhiRes","(#phi_{L1}-#phi_{Ref})/#phi_{Ref}","No. of entries"); c1->Update();
+  Plot(f,dir+"/Resolutions/PhiCor","#phi_{Ref} (rad)","#phi_{L1} (rad)"); c1->Update();
   Plot(f,dir+"/Efficiencies/EtEff","E_{T} (GeV)","Efficiency","e"); c1->Update();
   Plot(f,dir+"/Efficiencies/EtaEff","#eta","Efficiency","e"); c1->Update();
   Plot(f,dir+"/Efficiencies/PhiEff","#phi (rad)","Efficiency","e"); c1->Update();
   
+  f->Close();
+
+
+  //   // single pion plots (et resolution, eff vs eta/phi)
+  //   TFile *f = new TFile("single_pi.root");
+  //   TString dir = "";
+  
+  //   // make jet sum histograms
+  
+
+  //   f->close();
+  
+  //   // H -> 4 mu plots (turn on)
+  //   TFile *f = new TFile("H_4mu.root");
+  
+  //   f->close();
+  
+  //   // H -> 2 gamma plots (turn on)
+  //   TFile *f = new TFile("H_2gamma.root");
+  
+  //   f->close();
+  
+  //   // H -> 2 tau plots (turn on)
+  //   TFile *f = new TFile("H_2tau.root");
+  
+  //   f->close();
+  
+  //   // Z' -> dijets plots (turn on)
+  //   TFile *f = new TFile("ZprimeDijets.root");
+  
+  //   f->close();
+  
   ps->Close();
 
 }
+
+// void validation(TString dir="", TString fileName="")
+// {
+//   // Open the file
+//   TFile *f = new TFile(fileName);
+
+//   // First the simple histograms
+  
+//   Plot(f,dir+"/L1Candidates/Et","E_{T} (GeV)","No. of entries"); c1->Update();
+//   Plot(f,dir+"/L1Candidates/Eta","#eta","No. of entries"); c1->Update();
+//   Plot(f,dir+"/L1Candidates/Phi","#phi (rad)","No. of entries"); c1->Update();
+
+//   Plot(f,dir+"/RefCandidates/Et","E_{T} (GeV)","No. of entries"); c1->Update();
+//   Plot(f,dir+"/RefCandidates/Eta","#eta","No. of entries"); c1->Update();
+//   Plot(f,dir+"/RefCandidates/Phi","#phi (rad)","No. of entries"); c1->Update();
+ 
+//   Plot(f,dir+"/Resolutions/DeltaR","#Delta R (L1,Ref)","No. of entries"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtRes","(E_{T,L1}-E_{T,Ref})/E_{T,Ref}","No. of entries"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtCor","E_{T, Ref} (GeV)","E_{T, L1} (GeV)"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtProf","E_{T, Ref} (GeV)","(E_{T,L1}-E_{T,Ref})/E_{T,Ref}"); c1->Update();
+
+//   Plot(f,dir+"/Resolutions/EtaRes","(#eta_{L1}-#eta_{Ref})/#eta_{Ref}","No. of entries"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtaCor","#eta_{Ref}","#eta_{L1}"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtaCor","#eta_{Ref}","#eta_{L1}"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtaProf","#eta_{Ref}","(#eta_{L1}-#eta_{Ref})/#eta_{Ref}"); c1->Update();
+
+//   Plot(f,dir+"/Resolutions/PhiRes","(#phi_{L1}-#phi_{Ref})/#phi_{Ref}","No. of entries"); c1->Update();
+//   Plot(f,dir+"/Resolutions/PhiCor","#phi_{Ref} (rad)","#phi_{L1} (rad)"); c1->Update();
+//   Plot(f,dir+"/Resolutions/PhiProf","#phi_{Ref} (rad)","(#phi_{L1}-#phi_{Ref})/#phi_{Ref}"); c1->Update();
+
+//   ps->Close();
+
+// }
 
 void Plot(TFile* f, TString Hist, TString XAxisLabel, TString YAxisLabel="Events", TString Opt="")
 {
