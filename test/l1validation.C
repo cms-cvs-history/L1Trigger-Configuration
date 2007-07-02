0360805e75dfbdbcd9a41136ecd01ba6026e6d15
Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch>
2007-07-02 19:27:44 +0000

    validation config working

--- test/l1validation.C
+++ test/l1validation.C
@@ -22,26 +22,25 @@ void l1validation()
   TCanvas *c1 = new TCanvas("c1","c1",800,800);
   
   
-  //   // single muon plots (pt resolution, eff vs eta/phi)
-  //   TFile *f = new TFile("single_mu.root");
-  //   TString dir = "";
+  /// single muon plots ///
+//   TFile *f = new TFile("single_mu.root");
   
-  //   // resolutions
-  //   Plot(f,dir+"/Resolutions/EtRes","(E_{T,L1}-E_{T,Ref})/E_{T,Ref}","No. of entries"); c1->Update();
-  //   Plot(f,dir+"/Resolutions/EtCor","E_{T, Ref} (GeV)","E_{T, L1} (GeV)"); c1->Update();
-  //   Plot(f,dir+"/Resolutions/EtaRes","(#eta_{L1}-#eta_{Ref})/#eta_{Ref}","No. of entries"); c1->Update();
-  //   Plot(f,dir+"/Resolutions/EtaCor","#eta_{Ref}","#eta_{L1}"); c1->Update();
-  //   Plot(f,dir+"/Resolutions/PhiRes","(#phi_{L1}-#phi_{Ref})/#phi_{Ref}","No. of entries"); c1->Update();
-  //   Plot(f,dir+"/Resolutions/PhiCor","#phi_{Ref} (rad)","#phi_{L1} (rad)"); c1->Update();
+//   // muon resolutions & efficiencies
+//   TString dir = "L1AnalyzerMuonMC";
   
-  //   // efficiencies
-  //   Plot(f,dir+"/Efficiencies/EtEff","E_{T} (GeV)","Efficiency","e"); c1->Update();
-  //   Plot(f,dir+"/Efficiencies/EtaEff","#eta","Efficiency","e"); c1->Update();
-  //   Plot(f,dir+"/Efficiencies/PhiEff","#phi (rad)","Efficiency","e"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtRes","(E_{T,L1}-E_{T,Ref})/E_{T,Ref}","No. of entries"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtCor","E_{T, Ref} (GeV)","E_{T, L1} (GeV)"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtaRes","(#eta_{L1}-#eta_{Ref})/#eta_{Ref}","No. of entries"); c1->Update();
+//   Plot(f,dir+"/Resolutions/EtaCor","#eta_{Ref}","#eta_{L1}"); c1->Update();
+//   Plot(f,dir+"/Resolutions/PhiRes","(#phi_{L1}-#phi_{Ref})/#phi_{Ref}","No. of entries"); c1->Update();
+//   Plot(f,dir+"/Resolutions/PhiCor","#phi_{Ref} (rad)","#phi_{L1} (rad)"); c1->Update();
+//   Plot(f,dir+"/Efficiencies/EtEff","E_{T} (GeV)","Efficiency","e"); c1->Update();
+//   Plot(f,dir+"/Efficiencies/EtaEff","#eta","Efficiency","e"); c1->Update();
+//   Plot(f,dir+"/Efficiencies/PhiEff","#phi (rad)","Efficiency","e"); c1->Update();
+
+//   f->Close();
   
-  //   f->close();
-  
-  // single electron plots (et resolution, eff vs eta/phi)
+  /// single electron plots ///
   TFile *f = new TFile("single_e.root");
   
   // iso EM resolutions & efficiencies
@@ -69,35 +68,37 @@ void l1validation()
   Plot(f,dir+"/Efficiencies/EtEff","E_{T} (GeV)","Efficiency","e"); c1->Update();
   Plot(f,dir+"/Efficiencies/EtaEff","#eta","Efficiency","e"); c1->Update();
   Plot(f,dir+"/Efficiencies/PhiEff","#phi (rad)","Efficiency","e"); c1->Update();
+
+  // make iso/non-iso sum histograms
+  PlotCombinedEff(f,"L1AnalyzerIsoEmMC","L1AnalyzerNonIsoEmMC","", "Eta", "#eta","Efficiency","e"); c1->Update();
+
   
+  
+  //f->Close();
+
+
+  /// single pion plots ///
+  //  TFile *f = new TFile("single_pi.root");
+  
+
   f->Close();
-
-
-  //   // single pion plots (et resolution, eff vs eta/phi)
-  //   TFile *f = new TFile("single_pi.root");
-  //   TString dir = "";
   
-  //   // make jet sum histograms
-  
-
-  //   f->close();
-  
-  //   // H -> 4 mu plots (turn on)
+  /// H -> 4 mu plots ///
   //   TFile *f = new TFile("H_4mu.root");
   
   //   f->close();
   
-  //   // H -> 2 gamma plots (turn on)
+  /// H -> 2 gamma plots ///
   //   TFile *f = new TFile("H_2gamma.root");
   
   //   f->close();
   
-  //   // H -> 2 tau plots (turn on)
+  /// H -> 2 tau plots ///
   //   TFile *f = new TFile("H_2tau.root");
   
   //   f->close();
   
-  //   // Z' -> dijets plots (turn on)
+  /// Z' -> dijets plots ///
   //   TFile *f = new TFile("ZprimeDijets.root");
   
   //   f->close();
@@ -151,5 +152,32 @@ void Plot(TFile* f, TString Hist, TString XAxisLabel, TString YAxisLabel="Events
 
   // plot 
   H->Draw(Opt);
+
+}
+
+// Sum two/three L1 histos, divide by ref, and plot
+// Use for eg. total EM efficiency, rather than iso and non-iso separately
+void PlotCombinedEff(TFile* f, TString DirA, TString DirB, TString DirC, TString Hist, TString XAxisLabel, TString YAxisLabel="Events", TString Opt="")
+{
+
+  // Get the histograms from the files
+  TH1D * HistA = (TH1D*)f->Get(DirA+"/L1Candidates/"+Hist);
+  TH1D * HistB = (TH1D*)f->Get(DirB+"/L1Candidates/"+Hist);
+  TH1D * HistC;
+  if (!DirC.IsNull()) HistC = (TH1D*)f->Get(DirC+"/L1Candidates/"+Hist);
+  TH1D * Ref   = (TH1D*)f->Get(DirA+"/RefCandidates/"+Hist);
+  
+  //TH1D * H = new TH1D(*HistA);
+  //H->Add(HistB);
+  //  if (Ref->GetEntries() > 0) H->Divide(Ref);
+  
+  // Add the X axis label
+  //H->GetXaxis()->SetTitle(XAxisLabel);
+  //H->GetYaxis()->SetTitle(YAxisLabel);
+
+  // plot 
+  Ref->Draw(Opt);
+
+
 
 }
