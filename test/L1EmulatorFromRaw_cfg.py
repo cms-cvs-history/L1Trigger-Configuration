fb7597201dcc6635679bfbbe76ca4040da930c00
cmsbuild <>
2008-08-27 22:25:01 +0000

    This commit was manufactured by cvs2git to create tag 'L1Trigger- Configuration-V00-14-04'.
    Sprout from master 2008-08-07 23:14:14 UTC Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch> 'put config back in sequences'
    Cherrypick from master 2008-08-27 22:24:59 UTC Jim Brooke <sha1-afbdcd5b2849487bbb8e983abdb165a4c514f399@cern.ch> 'fix rerun workflow':
        python/L1StartupConfig_cff.py
        python/SimL1Emulator_cff.py
        test/L1EmulatorFromRaw_cfg.py

--- test/L1EmulatorFromRaw_cfg.py
+++ test/L1EmulatorFromRaw_cfg.py
@@ -3,27 +3,62 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("L1")
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
-process.load("L1Trigger.Configuration.L1Config_cff")
-process.load("Configuration.StandardSequences.RawToDigi_cff")
-process.load("L1Trigger.Configuration.SimL1Emulator_cff")
-
 process.source = cms.Source(
     "PoolSource",
-    fileNames = cms.untracked.vstring()
+    fileNames = cms.untracked.vstring(
+    '/store/relval/CMSSW_2_1_2/RelValQCD_Pt_80_120/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/IDEAL_V9_10TeV_v1/0005/162A3A26-5774-DD11-B0B6-001617C3B6DE.root',
+    )
 )
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(1)
+    input = cms.untracked.int32(-1)
 )
 
-process.ecalTriggerPrimitiveDigis.Label = 'ecalDigis'
-process.hcalTriggerPrimitiveDigis.inputLabel = 'hcalDigis'
-process.dtTriggerPrimitiveDigis.digiTag = 'muonDTDigis'
-process.cscTriggerPrimitiveDigis.CSCComparatorDigiProducer = 'muonCSCDigis'
-process.rpcTriggerDigis.label = 'muonRPCDigis'
+# standard includes
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "STARTUP_V6::All"
 
 
-process.p = cms.Path(process.ecalDigis*process.hcalDigis*process.muonDTDigis*process.muonCSCDigis*process.muonRPCDigis)
+# unpack raw data
+process.load("Configuration.StandardSequences.RawToDigi_cff")
 
-process.PoolSource.fileNames = ['/store/relval/2008/2/5/RelVal-QCD_Pt_80_120-1202115095-HLT/0000/0C84C079-D8D3-DC11-BD81-001617E30E28.root']
+# run trigger primitive generation on unpacked digis, then central L1
+process.load("L1Trigger.Configuration.CaloTriggerPrimitives_cff")
+process.load("L1Trigger.Configuration.SimL1Emulator_cff")
+process.simEcalTriggerPrimitiveDigis.Label = 'ecalDigis'
+process.simHcalTriggerPrimitiveDigis.inputLabel = 'hcalDigis'
+process.simDtTriggerPrimitiveDigis.digiTag = 'muonDTDigis'
+process.simCscTriggerPrimitiveDigis.CSCComparatorDigiProducer = cms.InputTag("muonCSCDigis","MuonCSCComparatorDigi")
+process.simCscTriggerPrimitiveDigis.CSCWireDigiProducer = cms.InputTag("muonCSCDigis","MuonCSCWireDigi")
+process.simRpcTriggerDigis.label = 'muonRPCDigis'
 
+# L1 configuration
+process.load('L1Trigger.Configuration.L1DummyConfig_cff')
+
+# optional trigger configurations
+#process.load('L1Trigger.Configuration.L1StartupConfig_cff')
+#process.load('L1Trigger.Configuration.L1CruzetConfig_cff')
+
+# Example for what collections are needed to re-run HLT
+process.out = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring(
+        'keep FEDRawDataCollection_*_*_L1',
+        'keep *_simGtDigis_*_L1',
+        'keep *_simGmtDigis_*_L1',
+        'keep *_simGctDigis_*_L1'
+    ),
+    fileName = cms.untracked.string("rerunL1.root")
+)
+
+process.p = cms.Path(
+    process.ecalDigis
+    *process.hcalDigis
+    *process.muonDTDigis
+    *process.muonCSCDigis
+    *process.muonRPCDigis
+    *process.CaloTriggerPrimitives
+    *process.SimL1Emulator
+)
+
+process.o = cms.EndPath( process.out )
